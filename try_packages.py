e7894efadb27edbd9d3f220f25c39c26269cc5ba
Robert Cimrman <cimrman3@ntc.zcu.cz>
2021-06-11 09:06:19 +0200

    try_packages.py: new --package, --form options
    - allow choices in generated options in main()
    - save options to output_dir, setup output
    - define default profile()
    - return elapsed times from assemble_sfepy_form(), assemble_fenics_form()

--- try_packages.py
+++ try_packages.py
@@ -9,14 +9,21 @@ import numpy as nm
 import fenics as fe
 from ffc.fiatinterface import create_quadrature as cquad
 
+from sfepy.base.base import output
 from sfepy.discrete.fem import FEDomain, Field
 from sfepy.discrete import (FieldVariable, Integral, Equation, Equations,
                             Problem)
 from sfepy.terms import Term
 from sfepy.mesh.mesh_generators import gen_block_mesh
 
-from soops import Struct
+import soops as so
 from soops.timing import Timer
+
+try:
+    _ = profile
+
+except NameError:
+    profile = lambda x: x
 
 def print_fenics_n_qp():
     shape = 'hexahedron'
@@ -26,7 +33,8 @@ def print_fenics_n_qp():
         points, weights = cquad(shape, deg, scheme)
         print('degree:', deg, 'n_qp:', len(points))
 
-def assemble_sfepy_form(n_cell, order, repeat):
+@profile
+def assemble_sfepy_form(form, n_cell, order, repeat):
     mesh = gen_block_mesh((n_cell, 1, 1), (n_cell + 1, 2, 2), (0, 0, 0),
                           name='')
     domain = FEDomain('el', mesh)
@@ -38,6 +46,7 @@ def assemble_sfepy_form(n_cell, order, repeat):
     v = FieldVariable('v', 'test', field, primary_var_name='u')
 
     timer = Timer()
+    times = []
     for ir in range(repeat):
         timer.start()
         integral = Integral('i', order=0)
@@ -47,14 +56,15 @@ def assemble_sfepy_form(n_cell, order, repeat):
         eqs = Equations([eq])
 
         pb = Problem('pb', equations=eqs)
-        mtx = pb.evaluate('dw_volume_dot.{}.omega(v, u)'.format(2 * order),
+        mtx = pb.evaluate('{}.{}.omega(v, u)'.format(form, 2 * order),
                           mode='weak', dw_mode='matrix')
-        # mtx = pb.evaluate('dw_laplace.{}.omega(v, u)'.format(2 * order),
-        #                   mode='weak', dw_mode='matrix')
-        print(timer.stop())
-        print(mtx.shape[0])
+        times.append(timer.stop())
+        output('repeat:', ir, mtx.shape[0], times[-1])
 
-def assemble_fenics_form(n_cell, order, repeat):
+    return times
+
+@profile
+def assemble_fenics_form(form, n_cell, order, repeat):
     mesh = fe.BoxMesh.create([fe.Point(0,0,0), fe.Point(n_cell, 1, 1)],
                              [n_cell, 1, 1],
                              fe.CellType.Type.hexahedron)
@@ -71,13 +81,20 @@ def assemble_fenics_form(n_cell, order, repeat):
     }
 
     timer = Timer()
+    times = []
     for ir in range(repeat):
         timer.start()
-        # term = fe.dot(fe.grad(u), fe.grad(v))*fe.dx
-        term = fe.dot(u, v)*fe.dx
+        if form == 'dw_laplace':
+            term = fe.dot(u, v)*fe.dx
+
+        elif form == 'dw_volume_dot':
+            term = fe.dot(fe.grad(u), fe.grad(v))*fe.dx
+
         mtx = fe.assemble(term, form_compiler_parameters=fcc_pars)
-        print(timer.stop())
-        print(mtx.size(0))
+        times.append(timer.stop())
+        output('repeat:', ir, mtx.size(0), times[-1])
+
+    return times
 
 helps = {
     'output_dir'
@@ -89,7 +106,9 @@ helps = {
 }
 
 def main():
-    opts = Struct(
+    opts = so.Struct(
+        package = ('sfepy', 'fenics'),
+        form = ('dw_laplace', 'dw_volume_dot'),
         n_cell = 1024,
         order = 1,
         repeat = 2,
@@ -100,16 +119,21 @@ def main():
     for key, val in opts.items():
         helps[key] = '[default: %(default)s]'
         action = 'store'
+        choices = None
         if val is True:
             action = 'store_false'
 
         elif val is False:
             action = 'store_true'
 
+        elif isinstance(val, tuple):
+            choices = val
+            val = val[0]
+
         if action == 'store':
             parser.add_argument('--' + key.replace('_', '-'),
                                 type=type(val),
-                                action=action, dest=key,
+                                action=action, dest=key, choices=choices,
                                 default=val, help=helps[key])
         else:
             parser.add_argument('--' + key.replace('_', '-'),
@@ -123,13 +147,28 @@ def main():
                         default=False, help=helps['shell'])
     options = parser.parse_args()
 
-    fe.set_log_active(False)
+    output_dir = options.output_dir
+    output.prefix = 'try_packages:'
+    filename = op.join(output_dir, 'output_log.txt')
+    so.ensure_path(filename)
+    output.set_output(filename=filename, combined=options.silent == False)
 
-    print_fenics_n_qp()
+    filename = op.join(output_dir, 'options.txt')
+    so.save_options(filename, [('options', vars(options))],
+                    quote_command_line=True)
 
-    assemble_fenics_form(options.n_cell, options.order, options.repeat)
-    assemble_sfepy_form(options.n_cell, options.order, options.repeat)
+    if options.package == 'sfepy':
+        times = assemble_sfepy_form(options.form, options.n_cell,
+                                    options.order, options.repeat)
 
+    elif options.package == 'fenics':
+        fe.set_log_active(False)
+
+        print_fenics_n_qp()
+        times = assemble_fenics_form(options.form, options.n_cell,
+                                     options.order, options.repeat)
+
+    output('times:', times)
     if options.shell:
         from soops.base import shell; shell()
 
